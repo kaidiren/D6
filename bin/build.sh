70d5b71ab7816686588dd3aff7acabf34d0db269
kaidiren <kaidiren@gmail.com>
2017-08-20 13:10:21 +0800

    auto update

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,5 @@
-hugo --theme=hugo-nuo --baseUrl="http://www.xli.me/" --buildDrafts 2> /dev/null
+#!/bin/sh
+hugo --theme=hugo-nuo --baseUrl="http://www.xli.me/" --buildDrafts
 cp -r public/* docs/
 git add .
 git commit -m'auto update'
